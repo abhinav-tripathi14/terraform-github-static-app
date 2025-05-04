8b6d4abacfe5b45cd244e45f95618d97d31ad8a2
Abhinav Tripathi <abhi.tripathi14@gmail.com>
2025-05-04 10:29:40 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "template_org" {
 
 variable "template_repo" {
   description = "The name of the repository in Github that contains the example app code."
-  default     = "terraform-github-static-app"
+  default     = "learn-hcp-waypoint-static-app-template"
 }
 
 variable "destination_org" {
